ae0600ce7f87d2d9877e580c73fa22629b144608
mihir <mihirdeogaonkar@gmail.com>
2026-03-29 14:30:41 +0530

    DrawSegment() now implemented

--- main.c
+++ main.c
@@ -5,6 +5,25 @@
 #define WIDTH 900
 #define HEIGHT 600
 
+float segment_width = 200;
+float segment_height = 50;
+
+void DrawSegment(Vector2 center){
+        // making the 7 segment display
+        int count = 6;
+
+        Vector2 a,b,c,d,e,f;
+        a = (Vector2){center.x - segment_width/2 - segment_height/2, center.y};
+        b = (Vector2){center.x - segment_width/2, center.y - segment_height/2};
+        c = (Vector2){center.x - segment_width/2, center.y + segment_height/2};
+        d = (Vector2){center.x + segment_width/2, center.y - segment_height/2};
+        e = (Vector2){center.x + segment_width/2, center.y + segment_height/2};
+        f = (Vector2){center.x + segment_width/2 + segment_height/2, center.y};
+
+        Vector2 points[] = {a,c,b,e,d,f};
+        DrawTriangleStrip(points, count, SKYBLUE);
+}
+
 int main(){
 
     time_t current_time = time(NULL);
@@ -15,26 +34,8 @@ int main(){
 
     while (!WindowShouldClose()){
         BeginDrawing();
-        ClearBackground(BLACK);
-        DrawRectangle(50,50,300,20,RAYWHITE);
-
-        // actual shit
-        int count = 6;
         Vector2 center = {WIDTH/2, HEIGHT/2};
-        float segment_width = 200;
-        float segment_height = 50;
-
-        Vector2 a,b,c,d,e,f;
-        a = (Vector2){center.x - segment_width/2 - segment_height/2, center.y};
-        b = (Vector2){center.x - segment_width/2, center.y - segment_height/2};
-        c = (Vector2){center.x - segment_width/2, center.y + segment_height/2};
-        d = (Vector2){center.x + segment_height/2, center.y - segment_height/2};
-        e = (Vector2){center.x + segment_width/2, center.y + segment_height/2};
-        f = (Vector2){center.x + segment_width/2 + segment_height/2, center.y};
-
-        Vector2 points[] = {a,b,c,d,e,f};
-        DrawTriangleStrip(points, count, SKYBLUE);
-
+        DrawSegment((Vector2){300, 200});
         EndDrawing();
     }
 
